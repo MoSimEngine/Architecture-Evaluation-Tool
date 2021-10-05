f2981c56cc89f225f8e2980c2a94cb03b2e74419
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-10-05 13:13:56 +0200

    style(*): change system node generation

--- src/main/java/org/mosim/refactorlizar/architecture_evaluation/graphs/CtTypeSystemGraphUtils.java
+++ src/main/java/org/mosim/refactorlizar/architecture_evaluation/graphs/CtTypeSystemGraphUtils.java
@@ -1,9 +1,11 @@
 package org.mosim.refactorlizar.architecture_evaluation.graphs;
 
+import java.util.Random;
+
 import com.google.common.graph.Graph;
 import com.google.common.graph.Graphs;
 import com.google.common.graph.MutableGraph;
-import java.util.UUID;
+
 import spoon.Launcher;
 import spoon.reflect.declaration.CtType;
 
@@ -13,7 +15,7 @@ public class CtTypeSystemGraphUtils implements SystemGraphUtils<CtType<?>> {
     public MutableGraph<Node<CtType<?>>> convertToSystemGraph(Graph<Node<CtType<?>>> graph) {
         MutableGraph<Node<CtType<?>>> systemGraph = Graphs.copyOf(graph);
         // add empty node as system node
-        CtType<?> type = Launcher.parseClass("class " + UUID.randomUUID() + " {}");
+        CtType<?> type = Launcher.parseClass("class SystemNode" + new Random().nextInt() + " {}");
         var method = new Launcher().createFactory().createMethod();
         type.addMethod(method);
         systemGraph.addNode(new SpoonNode(method));
